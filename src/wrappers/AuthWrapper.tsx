7cad7bdf3a957a00dc2c5eac871da4089d18c1db
Ten <Ten>
2022-11-12 00:24:22 +0200

    ZZMED-148: sign in

--- src/wrappers/AuthWrapper.tsx
+++ src/wrappers/AuthWrapper.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
+
 import { useIsAuth } from '~stores/slices/auth.slice'
 
 export const AuthWrapper = () => {
