fcfc7dc55d576160aac3ee769b91e7a6ba14fed4
Ten <Ten>
2022-12-09 15:10:11 +0200

    ZZMED-260: thresholds

--- src/pages/Patient/Vitals/patient-vitals.tsx
+++ src/pages/Patient/Vitals/patient-vitals.tsx
@@ -3,13 +3,9 @@ import React from 'react'
 
 import { Thresholds } from '~components/Thresholds/thresholds'
 
-export const PatientVitals = () => {
-  console.log('Vitals')
-
-  return (
-    <div className="white-box content-md">
-      <Typography variant="h5">Thresholds</Typography>
-      <Thresholds />
-    </div>
-  )
-}
+export const PatientVitals = () => (
+  <div className="white-box content-md">
+    <Typography variant="h5">Thresholds</Typography>
+    <Thresholds />
+  </div>
+)
