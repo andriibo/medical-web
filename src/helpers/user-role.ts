0c1cb7bd2f2438160158a49e70312f2bc0469a23
andrii <bondarenkoa1990@gmail.com>
2022-12-15 18:51:45 +0200

    ZZMED-289: Sign In for Caregiver

--- src/helpers/user-role.ts
+++ src/helpers/user-role.ts
@@ -1,11 +1,11 @@
 import { UserRoles } from '~/enums/user-roles.enum'
 
-export const isUserRoleGrantable = (role: string): boolean => {
+export const isUserRoleGrantable = (role: string) => {
   const grantableRoles = [UserRoles.caregiver, UserRoles.doctor]
 
   return grantableRoles.includes(role as UserRoles)
 }
 
-export const isUserRolePatient = (role: string): boolean => UserRoles.patient == role
+export const isUserRolePatient = (role: string) => UserRoles.patient === role
 
-export const isUserRoleDoctor = (role: string): boolean => UserRoles.doctor == role
+export const isUserRoleDoctor = (role: string) => UserRoles.doctor === role
