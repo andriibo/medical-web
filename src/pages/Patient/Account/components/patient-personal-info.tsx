e570cc3d420d321935fe04db1ba3c63557ca4da6
Ten <Ten>
2023-03-07 00:02:25 +0200

    ZZMED-437: vitals chart

--- src/pages/Patient/Account/components/patient-personal-info.tsx
+++ src/pages/Patient/Account/components/patient-personal-info.tsx
@@ -1,8 +1,7 @@
 import { Edit } from '@mui/icons-material'
 import { Button, Chip, Divider, IconButton, Typography } from '@mui/material'
 import dayjs from 'dayjs'
-import { mean, std } from 'mathjs'
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 
 import { DeleteAccountButton } from '~components/DeleteAccountButton/delete-account-button'
 import { EmptyBox } from '~components/EmptyBox/empty-box'
@@ -45,13 +44,6 @@ export const PatientPersonalInfo = () => {
   const handleChangePasswordPopupClose = () => {
     setChangePasswordPopupOpen(false)
   }
-
-  useEffect(() => {
-    const arr = [1, 8, 9, 8, 7, 8, 9, 8, 7, 8, 9, 8, 7, 8, 9, 8, 7, 20]
-
-    console.log(mean(arr))
-    console.log(std(arr))
-  }, [])
 
   if (isLoading) {
     return <Spinner />
