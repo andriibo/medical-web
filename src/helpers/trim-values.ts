0a6df214715aa7d990d635ae3f70147fdc18443f
Ten <Ten>
2023-03-28 16:21:19 +0300

    ZZMED-638: invite user

--- src/helpers/trim-values.ts
+++ src/helpers/trim-values.ts
@@ -1,11 +1,7 @@
 export function trimValues<K extends string>(values: Record<K, any>): Record<K, any> {
   const result: Partial<Record<K, any>> = values
 
-  console.log(values)
-
   for (const key in values) {
-    console.log(values[key])
-
     if (Boolean(values[key]) && typeof values[key] === 'string') result[key] = values[key].trim()
   }
 
