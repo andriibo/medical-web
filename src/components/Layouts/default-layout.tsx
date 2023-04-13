ab6bcb9b0af7b92a68f484840f188ea8b8f9e518
Ten <Ten>
2023-04-13 16:09:07 +0300

    ZZMED-705: last password update

--- src/components/Layouts/default-layout.tsx
+++ src/components/Layouts/default-layout.tsx
@@ -1,10 +1,11 @@
 import { Container } from '@mui/material'
-import React, { useEffect } from 'react'
+import React, { useCallback, useEffect } from 'react'
 import { Navigate, Outlet, useLocation } from 'react-router-dom'
 
 import { PageUrls } from '~/enums/page-urls.enum'
+import { IAuthData } from '~models/auth.model'
 import { useAppDispatch } from '~stores/hooks'
-import { useHasEmergencyContacts, useIsAuth, useUserDeletedAt } from '~stores/slices/auth.slice'
+import { setUserData, useHasEmergencyContacts, useIsAuth, useUserDeletedAt } from '~stores/slices/auth.slice'
 import { useEmergencyContactIsLoading } from '~stores/slices/emergency-contact.slice'
 import { combineApi } from '~stores/store'
 
@@ -25,6 +26,29 @@ export const DefaultLayout = () => {
       })
     }
   }, [dispatch, isAuth])
+
+  const handleStorage = useCallback(
+    (event: StorageEvent) => {
+      if (event.key === 'persist:root') {
+        const persistStorage = localStorage.getItem('persist:root')
+
+        if (persistStorage) {
+          const persistData = JSON.parse(JSON.parse(persistStorage).data) as IAuthData
+
+          dispatch(setUserData(persistData))
+        }
+      }
+    },
+    [dispatch],
+  )
+
+  useEffect(() => {
+    window.addEventListener('storage', handleStorage)
+
+    return () => {
+      window.removeEventListener('storage', handleStorage)
+    }
+  }, [handleStorage, dispatch])
 
   if (userDeletedAt && location.pathname !== PageUrls.AccountRecovery) {
     return <Navigate replace to={PageUrls.AccountRecovery} />
