a8884e050f95826f255c39387f2ee01fb99ba5f6
Ten <Ten>
2023-06-13 16:39:05 +0300

    ZZMED-905: calculate_chart_points

--- src/helpers/get-vitals-by-period.ts
+++ src/helpers/get-vitals-by-period.ts
@@ -37,7 +37,7 @@ export const getVitalsByPeriod = (vitals: IVital[], start: number, end: number):
   let interval = duration / INTERVALS_NUMBER
 
   if (!isLongDuration) {
-    startOffset = 1
+    startOffset = 1000
     interval = MIN_INTERVAL_DURATION
   }
 
