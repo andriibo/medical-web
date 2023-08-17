8fe3195ad1d6a3a639c938f0d494a060cd258e79
Ten <Ten>
2023-08-17 13:08:24 +0300

    feat(ZZMED-1112, ZZMED-1116): payload, hide zero dose

--- src/models/diagnoses.model.ts
+++ src/models/diagnoses.model.ts
@@ -1,8 +1,16 @@
+import { IUserModel } from '~models/user.model'
+
 export interface IDiagnosis {
   diagnosisId: string
   diagnosisName: string
   createdBy: string
   createdAt: string
+  createdByUser?: IUserModel
+}
+
+export interface IDiagnosisResponse {
+  diagnoses: IDiagnosis[]
+  users: IUserModel[]
 }
 
 export interface ICreateDiagnosisForm {
--- src/stores/services/patient-diagnosis.api.ts
+++ src/stores/services/patient-diagnosis.api.ts
@@ -1,7 +1,8 @@
 import { createApi } from '@reduxjs/toolkit/dist/query/react'
 import dayjs from 'dayjs'
 
-import { ICreateDiagnosis, IDiagnosis } from '~models/diagnoses.model'
+import { setTreatmentAuthor } from '~helpers/set-treatment-author'
+import { ICreateDiagnosis, IDiagnosis, IDiagnosisResponse } from '~models/diagnoses.model'
 import { staggeredBaseQueryWithBailOut } from '~stores/helpers/staggered-base-query-with-bail-out'
 
 export const patientDiagnosisApi = createApi({
@@ -17,8 +18,11 @@ export const patientDiagnosisApi = createApi({
       query: ({ patientUserId }) => ({
         url: `patient-diagnoses/${patientUserId}`,
       }),
-      transformResponse: (response: IDiagnosis[]) =>
-        response.sort((a, b) => dayjs(a.createdAt).unix() - dayjs(b.createdAt).unix()),
+      transformResponse: ({ diagnoses, users }: IDiagnosisResponse) => {
+        diagnoses.sort((a, b) => dayjs(a.createdAt).unix() - dayjs(b.createdAt).unix())
+
+        return setTreatmentAuthor(diagnoses, users)
+      },
       providesTags: ['PatientDiagnoses'],
     }),
     deletePatientDiagnosis: build.mutation<null, { diagnosisId: string }>({
